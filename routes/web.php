10545a223d827cd137e586bae5801ab0f83addf5
Pedro Kruszynski <pedro.nascimento39@etec.sp.gov.br>
2020-07-27 16:23:46 -0300

    first commit

---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Auth::routes();
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::group(['prefix' => 'interview'], function() {
+    Route::get('/',                                     'InterviewController@index')->name('interviews');
+    Route::get('/createAndAlterView/{id?}',             'InterviewController@createAndAlterView')->middleware('role:developer|administrador')->name('createAndAlterView');
+    Route::post('/save',                                'InterviewController@saveInterview')->middleware('role:developer|administrador')->name('saveInterview');
+    Route::put('/{id}',                                 'InterviewController@disableInterview')->middleware('role:developer|administrador')->name('disableInterview');
+    Route::delete('/{id}',                              'InterviewController@deleteInterview')->middleware('role:developer|administrador')->name('deleteInterview');
+});
